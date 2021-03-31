f354acd7c57f082fadf052763fac5201f629076c
seannlee123 <69945901+seannlee123@users.noreply.github.com>
2021-03-30 21:38:01 -0700

    model relationships and db fixed

--- Userlog/admin.py
+++ Userlog/admin.py
@@ -4,8 +4,9 @@ from django.contrib import admin
 # Register your models here.
 
 #Register model to admin site
-from .models import  UserTopic, Entry,Tag
+from .models import *
 admin.site.register(UserTopic)
 admin.site.register(Entry)
 admin.site.register(Tag)
+admin.site.register(customer)
 
--- Userlog/models.py
+++ Userlog/models.py
@@ -5,9 +5,18 @@ from django.db import models
 #first create the class
 #we need to make userclass
 
+class customer(models.Model):
+    name = models.CharField(max_length= 200, null = True)
+    phone = models.CharField(max_length= 200, null = True)
+    email = models.CharField(max_length= 200, null = True)
+    date_created = models.DateTimeField(auto_now_add=True, null= True)
+
+    def __str__(self):
+        return self.name
+
+
 class UserTopic(models.Model):
     #category for user to chose
-
     #User interest
     text = models.CharField(max_length=120, null =True) #we want to display characters with max 250
     date_added = models.DateTimeField(auto_now_add=True, null=True ) #allows realtime accuracy upon upload
@@ -17,13 +26,16 @@ class UserTopic(models.Model):
 
 
 class Tag(models.Model):
-    name = models.CharField(max_length=15, null=True)
-   
-   
+    tag_name = models.CharField(max_length=15, null=True)
 
+    def __str__(self):
+        return self.tag_name
+   
+   
 #entry model
 
 class Entry(models.Model):
+    cust_name = models.ForeignKey(customer, null= True, on_delete=models.SET_NULL)
     tags = models.ForeignKey(Tag, null = True, on_delete=models.CASCADE)
     topic = models.ForeignKey(UserTopic, null = True, on_delete=models.CASCADE)
     text = models.TextField()
@@ -31,8 +43,8 @@ class Entry(models.Model):
  
 class Meta:
     verbose_name_plural = 'entries'
-def __str__(self):
-    return self.text[:50]+"..."
+    def __str__(self):
+        return self.text[:50]+"..."
 # ...
 on_delete=models.DO_NOTHING,
 
